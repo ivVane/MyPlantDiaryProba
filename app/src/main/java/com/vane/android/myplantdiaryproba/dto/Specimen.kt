a46a35e2eff30a1041dd9951f7ab3232ab9e3ec4
ivVane <iv.vane@gmail.com>
2020-06-29 00:31:13 +0200

    Use @Exclude to not save a field to Firebase.

--- app/src/main/java/com/vane/android/myplantdiaryproba/dto/Specimen.kt
+++ app/src/main/java/com/vane/android/myplantdiaryproba/dto/Specimen.kt
@@ -1,5 +1,7 @@
 package com.vane.android.myplantdiaryproba.dto
 
+import com.google.firebase.firestore.Exclude
+
 data class Specimen(
     var plantName: String = "",
     var latitude: String = "",
@@ -7,9 +9,18 @@ data class Specimen(
     var description: String = "",
     var datePlanted: String = "",
     var specimenId: String = "",
-    var plantId: Int = 0,
-    var events: ArrayList<Event> = ArrayList<Event>()
+    var plantId: Int = 0
 ) {
+
+    private var _events: ArrayList<Event> = ArrayList<Event>()
+
+    var events: ArrayList<Event>
+        @Exclude get() {
+            return _events
+        }
+        set(value) {
+            _events = value
+        }
 
     override fun toString(): String {
         return "$plantName $description $latitude $longitude"
