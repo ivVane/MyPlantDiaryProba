dc1ada44db03e51b391d9d66311b7db651161c0f
ivVane <iv.vane@gmail.com>
2020-06-28 22:23:46 +0200

    Handle swipe Left, Right, Top, Bottom gesture events.

--- app/src/main/java/com/vane/android/myplantdiaryproba/MainActivity.kt
+++ app/src/main/java/com/vane/android/myplantdiaryproba/MainActivity.kt
@@ -2,17 +2,93 @@ package com.vane.android.myplantdiaryproba
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.widget.Toast
+import androidx.core.view.GestureDetectorCompat
 import com.vane.android.myplantdiaryproba.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var detector: GestureDetectorCompat
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
         }
+        detector = GestureDetectorCompat(this, DiaryGestureListener())
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        return if (detector.onTouchEvent(event)) {
+            true
+        } else {
+            super.onTouchEvent(event)
+        }
+    }
+
+    inner class DiaryGestureListener : GestureDetector.SimpleOnGestureListener() {
+
+        private val SWIPE_THRESHOLD = 100
+        private val SWIPE_VELOCITY_THRESHOLD = 100
+
+        override fun onFling(
+            downEvent: MotionEvent?,
+            moveEvent: MotionEvent?,
+            velocityX: Float,
+            velocityY: Float
+        ): Boolean {
+            var diffX = moveEvent?.x?.minus(downEvent!!.x) ?: 0.0F
+            var diffY = moveEvent?.y?.minus(downEvent!!.y) ?: 0.0F
+
+            return if (Math.abs(diffX) > Math.abs(diffY)) {
+                // This is a left or right swipe.
+                if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffX > 0) {
+                        // Right swipe.
+                        this@MainActivity.onSwipeRight()
+                    } else {
+                        // Left swipe
+                        this@MainActivity.onLeftSwipe()
+                    }
+                } else {
+                    super.onFling(downEvent, moveEvent, velocityX, velocityY)
+                }
+                true
+            } else {
+                // This is either a bottom or top swipe.
+                if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffY > 0) {
+                        this@MainActivity.onSwipeTop()
+                    } else {
+                        this@MainActivity.onSwipeBottom()
+                    }
+                    true
+                } else {
+                    super.onFling(downEvent, moveEvent, velocityX, velocityY)
+                }
+            }
+        }
+
+    }
+
+    private fun onSwipeBottom() {
+        Toast.makeText(this, "Bottom Swipe", Toast.LENGTH_LONG).show()
+    }
+
+    private fun onSwipeTop() {
+        Toast.makeText(this, "Top Swipe", Toast.LENGTH_LONG).show()
+    }
+
+    private fun onLeftSwipe() {
+        Toast.makeText(this, "Left Swipe", Toast.LENGTH_LONG).show()
+    }
+
+    private fun onSwipeRight() {
+        Toast.makeText(this, "Right Swipe", Toast.LENGTH_LONG).show()
     }
 }
