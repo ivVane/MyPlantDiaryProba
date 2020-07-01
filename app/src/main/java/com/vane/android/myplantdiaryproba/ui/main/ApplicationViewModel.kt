8f2f36f8c2415cb09e514255ae64d0d979eb4e6b
ivVane <iv.vane@gmail.com>
2020-07-01 19:38:25 +0200

    Add build dependency, and rename LocationViewModel to ApplicationViedModel.

--- app/src/main/java/com/vane/android/myplantdiaryproba/ui/main/ApplicationViewModel.kt
+++ app/src/main/java/com/vane/android/myplantdiaryproba/ui/main/ApplicationViewModel.kt
@@ -3,7 +3,7 @@ package com.vane.android.myplantdiaryproba.ui.main
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 
-class LocationViewModel(application: Application) : AndroidViewModel(application) {
+class ApplicationViewModel(application: Application) : AndroidViewModel(application) {
 
     private val locationLiveData = LocationLiveData(application)
     fun getLocationLiveData() = locationLiveData
